949398d039be66b10267413bf48dce4d8ef49b53
Tasha Hankewych <tash1207@bellsouth.net>
2014-02-23 13:36:33 -0500

    now using the google maps api to display the user's location

--- src/co/tashawych/where/MapActivity.java
+++ src/co/tashawych/where/MapActivity.java
@@ -1,15 +1,23 @@
 package co.tashawych.where;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 public class MapActivity extends Activity {
+	
+	GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map.setMyLocationEnabled(true);
 	}
 
 	@Override
